6aae98ce6fcf58f9007db9516904319b6b75d850
s-kon-field <s-kon@lib.hokudai.ac.jp>
2024-11-07 14:49:42 +0900

    sd

--- js/script.js
+++ js/script.js
@@ -25,20 +25,3 @@ $(function(){
   });
 });
 
-$('.slick01').slick({
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-      }
-    },
-    {
-      breakpoint: 640,
-      settings: {
-        arrows: false,
-        dots: false,
-      }
-    },
-  ]
-});
